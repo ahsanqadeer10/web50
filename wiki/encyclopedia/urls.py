fa333782b931b1b9ae30f30124f43a9c39938305
Ahsan Qadeer <ahsan.qadeer10@hotmail.com>
2021-08-31 20:10:24 +0500

    Entry, Index, and Search.

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("new", views.new, name="new"),
+    path("<str:title>/edit", views.edit, name="edit"),
+    path("search", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,10 @@
+from django.forms.fields import CharField
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +12,43 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        return HttpResponse(f'The entry {title} does not exist.')
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.capitalize(),
+            "entry": markdown2.markdown(entry)
+        })
+
+
+def new(request):
+    return HttpResponse("Add entry page")
+
+
+def edit(request, title):
+    return HttpResponse(f"Edit {title} page")
+
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST["q"]
+        entry = util.get_entry(query)
+        if entry is None:
+            entries = util.list_entries()
+            search_results = []
+            for item in entries:
+                if query in item:
+                    search_results.append(item)
+            print(search_results)
+            return render(request, "encyclopedia/search.html", {
+                "query": query,
+                "search_results": search_results
+            })
+        else:
+            return render(request, "encyclopedia/entry.html", {
+                "title": query.capitalize(),
+                "entry": markdown2.markdown(entry)
+            })
